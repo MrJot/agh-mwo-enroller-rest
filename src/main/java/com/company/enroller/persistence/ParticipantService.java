0017723a7eaa93e91f003944d9ad12ec7c7b8c25
student2 <student2@pcoip.ki.agh.edu.pl>
2018-05-13 13:47:15 +0200

    Added 'add user' funcionality

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -23,6 +24,12 @@ public class ParticipantService {
 		
 		
 		return (Participant)connector.getSession().get(Participant.class, login);
+	}
+
+	public void addParticipant(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
 	}
 
 }
